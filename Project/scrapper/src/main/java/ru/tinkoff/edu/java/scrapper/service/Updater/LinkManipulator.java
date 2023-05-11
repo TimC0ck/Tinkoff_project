b0f8a01e2ba65bf4da3b6dcf49527b659cf228e9
TimC0ck <LRE.Lynxx@gmail.com>
2023-05-11 18:47:55 +0500

    e

--- Project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/Updater/LinkManipulator.java
+++ Project/scrapper/src/main/java/ru/tinkoff/edu/java/scrapper/service/Updater/LinkManipulator.java
@@ -3,6 +3,7 @@ package ru.tinkoff.edu.java.scrapper.service.Updater;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.edu.java.scrapper.domain.repository.dto.Link;
+import ru.tinkoff.edu.java.scrapper.domain.repository.jpa.entity.LinkEntity;
 import ru.tinkoff.edu.java.scrapper.domain.repository.response.QuestionResponse;
 import ru.tinkoff.edu.java.scrapper.domain.repository.response.RepositoryResponse;
 import ru.tinkoff.edu.java.parser.parsers.GitHubLinkParser;
@@ -30,6 +31,28 @@ public class LinkManipulator {
         }
         Link link = new Link();
         link.setLink(url);
+        link.setLastUpdate(OffsetDateTime.now());
+        if (record instanceof GitHubRecord) {
+            RepositoryResponse response = gitHubClient.getRepoInfo(((GitHubRecord) record).username(),
+                    ((GitHubRecord) record).repo());
+            link.setLastActivity(response.updated_at());
+            link.setOpenIssuesCount(response.open_issues_count());
+        }
+        if (record instanceof StackOverflowRecord) {
+            QuestionResponse response = stackOverflowClient.getQuestionInfo(((StackOverflowRecord) record).questionId());
+            link.setLastActivity(response.last_activity_date());
+            link.setAnswerCount(response.answer_count());
+        }
+        return link;
+    }
+
+    public LinkEntity createLinkEntity(URI url) {
+        Record record = stackOverflowLinkParser.parseLink(url.toString());
+        if (record == null) {
+            throw new RuntimeException("Invalid link '" + url + "'");
+        }
+        LinkEntity link = new LinkEntity();
+        link.setLink(url.toString());
         link.setLastUpdate(OffsetDateTime.now());
         if (record instanceof GitHubRecord) {
             RepositoryResponse response = gitHubClient.getRepoInfo(((GitHubRecord) record).username(),
